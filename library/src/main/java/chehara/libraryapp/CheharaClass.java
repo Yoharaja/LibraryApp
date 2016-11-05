81e33138b5acda55fa87a89860e6c49b4bb0aa43
Yoharaja <yoharaja.dhanabalan@chehara.com>
2016-11-05 15:18:48 +0530

    Log in

--- library/src/main/java/chehara/libraryapp/CheharaClass.java
+++ library/src/main/java/chehara/libraryapp/CheharaClass.java
@@ -2,6 +2,7 @@ package chehara.libraryapp;
 
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 
 /**
  * Created by answerz on 3/11/16.
@@ -11,5 +12,9 @@ public class CheharaClass {
     public static  void callIntent(Context context){
         Intent intent = new Intent(context, MainLIBActivity.class);
         context.startActivity(intent);
+    }
+
+    public static void log(){
+        Log.e("TAG","ok");
     }
 }
